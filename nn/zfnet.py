1de02e2aad26c5499c5a08819fb37089871f2538
smirk <smirk.cao@gmail.com>
2018-07-27 19:11:13 +0800

    Building ZFNet with Keras Note: 1. input size 1. padding method

---
+++ nn/zfnet.py
@@ -0,0 +1,58 @@
+# -*-coding:utf-8-*-
+# Project:  nn
+# Filename: zfnet
+# Date: 7/27/18
+# Author: 😏 <smirk dot cao at gmail dot com>
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+import numpy as np
+
+# note：
+# 1. input size
+# 1. padding parameter in following layers
+
+
+if __name__ == '__main__':
+    # set fix random seed
+    seed = 2018
+    np.random.seed(seed)
+
+    model = Sequential()
+    # #INPUT
+    # input_size = (225, 225,3)
+
+    # #CONV 1
+    model.add(Conv2D(96, (7, 7), strides=(2, 2), input_shape=(225, 225, 3), padding='valid', activation='relu',
+                     kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same"))
+
+    # #CONV 2
+    model.add(Conv2D(256, (5, 5), strides=(2, 2), padding='valid', activation='relu', kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same"))
+
+    # #CONV 3
+    model.add(Conv2D(384, (3, 3), strides=(1, 1), padding='same', activation='relu', kernel_initializer='uniform'))
+
+    # #CONV 4
+    model.add(Conv2D(384, (3, 3), strides=(1, 1), padding='same', activation='relu', kernel_initializer='uniform'))
+
+    # #CONV 5
+    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu', kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+
+    model.add(Flatten())
+
+    # #FC 6
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+
+    # #FC 7
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+
+    # #OUTPUT
+    model.add(Dense(1000, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    model.summary()
