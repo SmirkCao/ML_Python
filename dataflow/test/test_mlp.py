231ba049012ef4e0335b14e25ebfbdec967783bb
smirk <smirk.cao@gmail.com>
2018-10-30 15:01:40 +0800

    travis update

--- dataflow/test/test_mlp.py
+++ dataflow/test/test_mlp.py
@@ -1,26 +1,34 @@
 # -*-coding:utf-8-*-
 # Project: ML_Python  
-# Filename: unit_test
+# Filename: test_mlp.py
 # Author: 😏 <smirk dot cao at gmail dot com>
-from mlp import *
+import sys
+import os
+p = os.path.join(os.getcwd(), "../../")
+sys.path.append(p)
+
+from dataflow.examples.mlp import *
 import numpy as np
+import unittest
 
 
-def test_mlp():
-    X = np.array([[0, 0, 1],
-                  [0, 1, 1],
-                  [1, 0, 1],
-                  [1, 1, 1]])
+class Test_MLP(unittest.TestCase):
 
-    y = np.array([[0],
-                  [1],
-                  [1],
-                  [0]])
-    mlpc = MLP(n_layers=7, hidden_layer_sizes=[10, 5, 4])
-    mlpc.fit(X, y)
-    print(mlpc.predict(X))
-    return mlpc
+    def test_mlp(self):
+        X = np.array([[0, 0, 1],
+                      [0, 1, 1],
+                      [1, 0, 1],
+                      [1, 1, 1]])
+
+        y = np.array([[0],
+                      [1],
+                      [1],
+                      [0]])
+        mlpc = MLP(n_layers=7, hidden_layer_sizes=[10, 5, 4])
+        mlpc.fit(X, y)
+        print(mlpc.predict(X))
+        return mlpc
 
 
 if __name__ == '__main__':
-    rst = test_mlp()
+    unittest.main()
