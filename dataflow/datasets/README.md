005ad53290c5208fb29c2a85a0c6cd372349a84e
Smirk <smirk.cao@gmail.com>
2018-11-16 15:00:33 +0800

    update dataset; add README.md

---
+++ dataflow/datasets/README.md
@@ -0,0 +1,17 @@
+# Gallery
+
+## Dummy
+
+## Line
+
+![](assert/gallery_line.png)
+
+## Curve
+
+![](assert/gallery_curve.png)
+
+## XOR
+
+## XOR3D
+
+## Mnist
--- dataflow/test/test_datasets.py
+++ dataflow/test/test_datasets.py
@@ -7,16 +7,42 @@
 import logging
 import sys
 import os
+
 p = os.path.join(os.getcwd(), "../../")
 sys.path.append(p)
 from dataflow.datasets.dummy import *
 import unittest
+import matplotlib.pyplot as plt
 
 
 class TestDatasets(unittest.TestCase):
     def test_mnist(self):
         load_mnist()
         pass
+
+    def test_line(self):
+        x, y = load_line()
+        plt.scatter(x, y)
+        # plt.show()
+
+    def test_curve(self):
+        x, y = load_curve1()
+        plt.scatter(x, y)
+        # plt.show()
+
+    def test_build_gallery(self):
+        # line
+        x, y = load_line()
+        plt.scatter(x, y, marker="$😏$", label="Smirk", s=150, linewidths=0.1)
+        plt.legend(loc='upper left')
+        plt.savefig("../datasets/assert/gallery_line.png")
+
+        # curve
+        plt.clf()
+        x, y = load_curve1()
+        plt.scatter(x, y, marker="$😏$", label="Smirk", s=150, linewidths=0.1)
+        plt.legend(loc='upper left')
+        plt.savefig("../datasets/assert/gallery_curve.png")
 
 
 if __name__ == '__main__':
