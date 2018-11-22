da9396ec3faccd40d045eff7309f7923159171a7
Smirk <smirk.cao@gmail.com>
2018-11-23 00:57:32 +0800

    gallery for curve1 regression

--- models/pytorch/mlp.py
+++ models/pytorch/mlp.py
@@ -13,6 +13,8 @@ from dataflow.datasets.dummy import load_curve1
 import matplotlib.pyplot as plt
 import logging
 import torch
+import glob
+import imageio
 
 
 class Net(torch.nn.Module):
@@ -53,24 +55,39 @@ if __name__ == '__main__':
     plt.ion()
     plt.show()
 
-    for n_iter in range(1000):
+    for n_iter in range(1500):
         pred = net(x)
         loss = loss_fn(pred, y)
         # 重要
         opt.zero_grad()
         loss.backward()
         opt.step()
-        if n_iter % 10 == 0:
+        if n_iter % 30 == 0:
             logger.info(loss.data.numpy())
             try:
                 ax.texts.remove(text_loss)
+                ax.texts.remove(text_iter)
             except NameError:
                 pass
 
             ax.plot(x.data.numpy(), pred.data.numpy(), "-r", alpha=0.2)
-            text_loss = plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
-            plt.pause(0.1)
+            text_loss = plt.text(0.5, 0, 'loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
+            text_iter = plt.text(0.5, -1, 'n_iter=%d' % n_iter, fontdict={'size': 20, 'color': 'red'})
+            plt.savefig(str(n_iter)+".png")
+            # plt.pause(0.1)
     # plt.waitforbuttonpress()
+    # generate gif
+    ofname = "mlp_pytorch.gif"
+    filenames = sorted(glob.glob("*.png"), key=os.path.getmtime, reverse=False)
+    filenames = [os.getcwd() + "/" + fname for fname in filenames]
+    if len(filenames) > 0:
+        frames = [imageio.imread(img_name) for img_name in filenames]
+        imageio.mimsave(ofname, frames, 'GIF', duration=0.1)
+
+    # clear png file
+    fs = glob.glob(r"*.png")
+    for f in fs:
+        os.remove(f)
 
 else:
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
