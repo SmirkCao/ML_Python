1d30f0d7f214472f6e8f8eaf68aa2ebd9a4d417c
Smirk <smirk.cao@gmail.com>
2018-11-27 02:46:08 +0800

    seperate rl and env

--- rl/treasure_on_right.py
+++ rl/treasure_on_right.py
@@ -1,28 +1,86 @@
 # ref to:
-# https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow/tree/master/contents/1_command_line_reinforcement_learning
+# https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow/
 # all in one demo to rl
 import logging
 import pandas as pd
 import numpy as np
 
 N_STATES = 6
-ACTIONS = ["left", "right"]
-ALPHA = 0.1  # learning rate
-MAX_EPISODES = 13
-EPSILON = 0.1  # epsilon-greddy
-GAMMA = 0.9
 
 
-def choose_action(s, q_table):
-    # epsilon = 0, greedy
-    state_actions = q_table.iloc[s, :]
-    if (np.random.uniform() < EPSILON) or ((state_actions == 0).all()):
-        # exploring
-        a = np.random.choice(ACTIONS)
-    else:
-        # exploiting
-        a = state_actions.idxmax()  # greedy
-    return a
+class RL(object):
+    def __init__(self,
+                 max_episodes=13,
+                 actions=[],
+                 alpha=0.1,
+                 gamma=0.9):
+        self.max_episodes = max_episodes
+        self.actions = actions
+        self.alpha = alpha
+        self.gamma = gamma
+
+    def choose_action(self, s):
+        raise NotImplemented
+
+    def learn(self):
+        raise NotImplemented
+
+
+class EpsilonGreddyQ(RL):
+    def __init__(self,
+                 max_episodes=13,
+                 actions=[],
+                 alpha=0.1,
+                 epsilon=0.1,
+                 gamma=0.9,
+                 n_states=None):
+        super().__init__(max_episodes=max_episodes,
+                         actions=actions,
+                         alpha=alpha,
+                         gamma=gamma)
+        self.epsilon = epsilon
+        self.q_table = pd.DataFrame(np.zeros((n_states, len(self.actions))),
+                                    columns=self.actions)
+
+    def choose_action(self, s):
+        # epsilon greedy
+        # epsilon = 0, greedy
+        state_actions = self.q_table.iloc[s, :]
+        if (np.random.uniform() < self.epsilon) or ((state_actions == 0).all()):
+            # exploring
+            a = np.random.choice(self.actions)
+        else:
+            # exploiting
+            a = state_actions.idxmax()  # greedy
+        return a
+
+    def learn(self):
+        np.random.seed(2)
+        for episode in range(self.max_episodes):
+            step_counter = 0
+            s = 0
+            is_terminated = False
+            # update_env(s, episode, step_counter)
+            while not is_terminated:
+                a = self.choose_action(s)
+                s_, r = get_env_feedback(s, a)
+
+                q_pred = self.q_table.loc[s, a]
+
+                if s_ != "terminal":
+                    q_target = r + self.gamma * self.q_table.iloc[s_, :].max()
+                else:
+                    q_target = r
+                    is_terminated = True
+
+                self.q_table.loc[s, a] += self.alpha * (q_target - q_pred)
+                s = s_
+
+                step_counter += 1
+                # update_env(s, episode, step_counter)
+            logger.info("episode: %d, step: %d" % (episode, step_counter))
+
+        return self.q_table
 
 
 def get_env_feedback(s, a):
@@ -55,39 +113,10 @@ def update_env(state, episode, step_counter):
     env_list = ["-"] * (N_STATES - 1) + ["T"]
 
 
-def rl():
-    np.random.seed(2)
-    q_table = pd.DataFrame(np.zeros((N_STATES, len(ACTIONS))), columns=ACTIONS)
-    for episode in range(MAX_EPISODES):
-        step_counter = 0
-        s = 0
-        is_terminated = False
-        # update_env(s, episode, step_counter)
-        while not is_terminated:
-            a = choose_action(s, q_table)
-            s_, r = get_env_feedback(s, a)
-
-            q_pred = q_table.loc[s, a]
-
-            if s_ != "terminal":
-                q_target = r + GAMMA * q_table.iloc[s_, :].max()
-            else:
-                q_target = r
-                is_terminated = True
-
-            q_table.loc[s, a] += ALPHA * (q_target - q_pred)
-            s = s_
-
-            step_counter += 1
-            # update_env(s, episode, step_counter)
-        logger.info("episode: %d, step: %d" % (episode, step_counter))
-
-    return q_table
-
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
 
-    q_table = rl()
+    Q = EpsilonGreddyQ(actions=["left", "right"], n_states=6)
+    q_table = Q.learn()
     logger.info(q_table)
